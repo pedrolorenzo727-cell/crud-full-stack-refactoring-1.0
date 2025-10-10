c34387e19cfd6e1d7903a56d32468e70c169d680
Pedro <pedro.lorenzo727@gmail.com>
2025-10-10 12:21:30 +0000

    Paginacion Materias Estudiantes

--- backend/repositories/studentsSubjects.php
+++ backend/repositories/studentsSubjects.php
@@ -23,6 +23,23 @@ function assignSubjectToStudent($conn, $student_id, $subject_id, $approved)
     ];
 }
 
+function getPaginatedSubjets($conn, $limit, $offset) 
+{
+    $stmt = $conn->prepare("SELECT * FROM students_subjets LIMIT ? OFFSET ?");
+    $stmt->bind_param("ii", $limit, $offset);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
+
+//2.0
+function getTotalSubjets($conn) 
+{
+    $sql = "SELECT COUNT(*) AS total FROM students_subjets";
+    $result = $conn->query($sql);
+    return $result->fetch_assoc()['total'];
+}
+
 //Query escrita sin ALIAS resumidos (a mi me gusta más):
 function getAllSubjectsStudents($conn) 
 {
